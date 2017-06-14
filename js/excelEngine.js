ff8ce497133cafed10421bff1f449efc8aa99e60
Happymilk <happymilk853@gmail.com>
2017-06-14 20:59:16 +0300

    excel: nav

--- js/excelEngine.js
+++ js/excelEngine.js
@@ -1,9 +1,9 @@
 const Excel = require('exceljs');
 
 let workbook = new Excel.Workbook();
-workbook.xlsx.readFile("../НСИ.xlsx").then(
+workbook.xlsx.readFile("НСИ.xlsx").then(
 	function() {
 		workbook.eachSheet(function(worksheet, sheetId) {
-    		
+			window.document.getElementById("navvv").innerHTML += '<li><a class="page-scroll">' + worksheet.name + '</a></li>';
 		});
     });
