4c673eb5d1245cc0bed4cd0f461f526a827b7180
Happymilk <happymilk853@gmail.com>
2017-06-15 01:46:00 +0300

    add table view & bar link

--- js/excelEngine.js
+++ js/excelEngine.js
@@ -1,13 +1,54 @@
 const Excel = require('exceljs');
+let indexes = [null];
 
 let workbook = new Excel.Workbook();
 workbook.xlsx.readFile("НСИ.xlsx").then(
 	function() {
+		let index = 1;
 		workbook.eachSheet(function(worksheet, sheetId) {
-			window.document.getElementById("navvv").innerHTML += '<li><a class="page-scroll">' + worksheet.name + '</a></li>';
-		});
+			window.document.getElementById("navvv").innerHTML += '<li><a class="page-scroll" onclick="execute('+ index + ')">' + worksheet.name + '</a></li>';
+			indexes.push(sheetId);
+			index++;
+		});	
 
-		window.document.getElementById('loader').style.display = 'none';
-		window.document.getElementById('p1').style.display = 'none';
-		window.document.getElementById('p2').style.display = 'none';
+		execute(1);
 });
+
+function execute(sheet) {
+	window.document.getElementById('loader').style.display = 'block';
+	window.document.getElementById('p1').style.display = 'block';
+	window.document.getElementById('p2').style.display = 'block';
+
+	let worksheet = workbook.getWorksheet(indexes[sheet]);
+	let rowcount = 0;
+	worksheet.getColumn(1).eachCell(function(cell, rowNumber) {
+		rowcount = rowNumber;
+	});
+	let max = 0;
+	worksheet.eachRow(function(row, rowNumber) {
+		row.eachCell(function(cell, colNumber) {
+			if (max < colNumber)
+				max = colNumber;
+		});
+	});
+	let string = '<tbody>';
+	for (let i = 1; i <= rowcount; i++) {
+		string += '<tr>'
+		let row = worksheet.getRow(i);
+		for (let j = 1; j <= max; j++)
+			if (row.getCell(j).value != null)
+				string += '<td>' + row.getCell(j).value + '</td>';
+			else
+				string += '<td></td>';
+			
+		string += '</tr>'
+	}
+	string += '</tbody>';
+	window.document.getElementById("firstTable").innerHTML = string;
+
+	window.document.getElementById('loader').style.display = 'none';
+	window.document.getElementById('p1').style.display = 'none';
+	window.document.getElementById('p2').style.display = 'none';
+}
+
+
