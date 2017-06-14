69150af0fd71fa918309e454df86461a6d7092c1
Happymilk <happymilk853@gmail.com>
2017-06-14 23:51:00 +0300

    add loading

--- js/excelEngine.js
+++ js/excelEngine.js
@@ -6,4 +6,8 @@ workbook.xlsx.readFile("НСИ.xlsx").then(
 		workbook.eachSheet(function(worksheet, sheetId) {
 			window.document.getElementById("navvv").innerHTML += '<li><a class="page-scroll">' + worksheet.name + '</a></li>';
 		});
-    });
+
+		window.document.getElementById('loader').style.display = 'none';
+		window.document.getElementById('p1').style.display = 'none';
+		window.document.getElementById('p2').style.display = 'none';
+});
